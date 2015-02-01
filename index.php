b8ed3e8d0e331a2b7fc4bb1727c94325dd8019e1
Avi Tapp (Desktop@Home) <Andrew.tapp@gmail.com>
2015-02-01 01:24:35 -0500

    Made changes to index.php, adding name tags, etc.

--- index.php
+++ index.php
@@ -8,23 +8,23 @@
     </head>
 
     <body>
-        <article class="container_12">
+        <form class="container_12" action="php/placeOrder.php" method="post">
             <fieldset class="grid_12">
                 <legend><h1>Purim Greeting Order Form</h1></legend>
                 <hr>
-                <div class="grid_6"><label>Name of Sender: </label><br><input></div>
-                <div class="grid_6"><label>Email Address: </label><br><input></div>
-                <div class="grid_6"><label>Address: </label><br><input></div>
-                <div class="grid_6"><label>Phone: </label><br><input></div>
+                <div class="grid_6"><label>Name of Sender: </label><br><input name="senderName"></div>
+                <div class="grid_6"><label>Email: </label><br><input name="senderEmail"></div>
+                <div class="grid_6"><label>Address: </label><br><input name="senderAddress"></div>
+                <div class="grid_6"><label>Phone: </label><br><input name="senderPhone"></div>
                 <hr>
-                <p>Please check the names of the families to whom you wish to send a personalized Purim Greeting card. Feel free to add any additional names and addresses. Kindly make your check payable to Saranac Synagogue Sisterhood and send it to 100 Delsan Ct., Buffalo, NY 14216 no later than March 6th, 2015.</p>
+                <p>Please check the names of the families to whom you wish to send a personalized Purim Greeting card. If you wish to add any additional families, contact us by email.</p>
         <?php
             $community = array();
             $community["Abramowitz, Mr./Mrs. Phil"] = "177 The Paddock, Williamsville, NY 14221";
             $community["Alt, Dr./Mrs. Allen"] = "10 Avon Rd., Binghamton, NY 13905";
             $community["Alt, Mr./Mrs. Robert"] = "281 Middlesex Rd., Buffalo, NY 14216";
             foreach($community as $name => $address){
-                print '<input type="checkbox" name="family" value="'.$name.'" /> '.$name.'<br>';
+                print '<input type="checkbox" name="jews[]" value="'.$name.'" /> '.$name.'<br>';
             }
         ?>
                 <hr>
@@ -33,14 +33,14 @@
                 <div class="grid_3"><label>($6) Outside The US</label><br><input id="costNonUS" readonly></div>
                 <div class="grid_3"><label>TOTAL</label><br><input id="costTotal" readonly></div>
                 <hr>
-                <div class="grid_9"><label>How would you like the cards signed? </label><input></div>
+                <div class="grid_9"><label>How would you like the cards signed? </label><input name="signed"></div>
                 <div class="grid_3"><button type="submit">SUBMIT</button></div>
                 <hr>
                 <div class="grid_12">
                     <ul id="hFamilies"></ul>
                 </div>
             </fieldset>
-        </article>
+        </form>
         <script src="js/main.js"></script>
     </body>
 </html>
